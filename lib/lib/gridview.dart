375aeee382295df7a730d5fc443ff90ade2c8324
JITHU MATHEWS <jithumathews111@gmail.com>
2023-01-14 10:14:39 +0530

    commit

--- lib/lib/gridview.dart
+++ lib/lib/gridview.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_draggable_gridview/flutter_draggable_gridview.dart';
+
 import 'images.dart';
 
 void main() {
@@ -21,12 +22,11 @@ class GridExample extends StatefulWidget {
 }
 
 class GridExampleState extends State<GridExample> {
-  List<DraggableGridItem> _listOfDraggableGridItem = [];
+  List<DraggableGridItem> _DraggableGridItem = [];
 
-  //method that is called when an object for your stateful widget is created and inserted inside the widget tree.
   @override
   void initState() {
-    _generateImageData();
+    _ImageData();
     super.initState();
   }
 
@@ -34,31 +34,24 @@ class GridExampleState extends State<GridExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
         title: Text(
           widget.title,
+          style: TextStyle(color: Colors.black),
         ),
       ),
       body: SafeArea(
         child: DraggableGridViewBuilder(
-          //A delegate that controls the layout of the children within the GridView.
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //Creates grid layouts with a fixed number of tiles in the cross axis.
             crossAxisCount: 2,
             childAspectRatio: MediaQuery.of(context).size.width /
                 (MediaQuery.of(context).size.height / 3),
           ),
-          /*This property contains list of [DraggableGridItem] and it is use to show the widget
-            inside the GridView.builder to provide the drag & drop functionality.
-            Also, it contains isDraggable parameter which manages enable/disable the drag & drop functionality. */
-          children: _listOfDraggableGridItem,
-          //This callback provides updated list and old and new indexes.
-          dragCompletion: onDragAccept,
-          /*This property contains bool value. If this property is false then it works with simple press draggable
-          or else it works with long press. default value is 'true'.*/
+          children: _DraggableGridItem,
+          dragCompletion: onDragIndex,
           isOnlyLongPress: false,
-          /* display the widget when the widget is being dragged. */
           dragFeedback: feedback,
-          //With this callback, you have to return a PlaceHolderWidget and we will use this widget in place holder.
           dragPlaceHolder: placeHolder,
         ),
       ),
@@ -76,22 +69,22 @@ class GridExampleState extends State<GridExample> {
   PlaceHolderWidget placeHolder(List<DraggableGridItem> list, int index) {
     return PlaceHolderWidget(
       child: Container(
-        color: Colors.white,
+        color: Colors.red,
       ),
     );
   }
 
-  void onDragAccept(
+  void onDragIndex(
       List<DraggableGridItem> list, int beforeIndex, int afterIndex) {
-    log('onDragAccept: $beforeIndex -> $afterIndex');
+    print('onDragAccept: $beforeIndex -> $afterIndex');
   }
 
-  void _generateImageData() {
-    _listOfDraggableGridItem.addAll(
+  void _ImageData() {
+    _DraggableGridItem.addAll(
       [
         DraggableGridItem(
           child: Container(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal: 4.0,
               vertical: 4.0,
             ),
@@ -104,7 +97,7 @@ class GridExampleState extends State<GridExample> {
         ),
         DraggableGridItem(
             child: Container(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 4.0,
                 vertical: 4.0,
               ),
@@ -116,7 +109,7 @@ class GridExampleState extends State<GridExample> {
             isDraggable: true),
         DraggableGridItem(
             child: Container(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 4.0,
                 vertical: 4.0,
               ),
@@ -128,120 +121,12 @@ class GridExampleState extends State<GridExample> {
             isDraggable: true),
         DraggableGridItem(
             child: Container(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 4.0,
                 vertical: 4.0,
               ),
               child: Image.asset(
                 Images.asset_4,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_5,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: false),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_6,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_7,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_8,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_9,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_10,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_11,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_12,
-                fit: BoxFit.cover,
-              ),
-            ),
-            isDraggable: true),
-        DraggableGridItem(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 4.0,
-                vertical: 4.0,
-              ),
-              child: Image.asset(
-                Images.asset_13,
                 fit: BoxFit.cover,
               ),
             ),
